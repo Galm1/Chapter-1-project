ed318fb94ef25ecbb5ea86c82fd2640b50bd51da
EliGarcia <garciaelisha93@yahoo.com>
2020-05-09 11:17:12 -0500

    First commit. Old and simple project. Using it to get used to github again!

---
+++ Chapter1PPElishaGarcia/src/Chapter1PPElishaGarcia/Chapter1PPElishaGarcia.java
@@ -0,0 +1,39 @@
+package Chapter1PPElishaGarcia;
+
+/**Student Name: Elisha Garcia
+
+*Date Completed: 01/29/2020
+
+*Project No. and Page No. in the book: Project 1, pg. 1 - 54
+
+*Extra Credit: No
+
+*Project Description: Displaying several lines that show my favorite movies. Project purpose is
+*					  to show that we can effectively use the basics of java and eclipse.
+
+**/
+
+
+//declaring my class
+public class Chapter1PPElishaGarcia {
+	
+	//creating the "main" method. Utilizing String[]
+	public static void main(String[] args) {
+		
+		//System.out.println("") allows us to print a string of text follows by a line.
+		System.out.println("-----------My Favorite Movies-----------");
+		//leaving the quotations in System.out.println("") empty allows us to create a black line.
+		System.out.println("");
+		System.out.println("");
+		//adding a tab in System.out.println("") also adds a tab in the programs output. 
+		System.out.println("	There Will Be Blood");
+		System.out.println("");
+		System.out.println("	Pulp Fiction");
+		System.out.println("");
+		System.out.println("	Punch Drunk Love");
+		System.out.println("");
+		System.out.println("	Old Boy");
+		
+	}
+
+}
